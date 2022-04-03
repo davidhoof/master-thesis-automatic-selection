b69aca315ee6f4db7979022e15ffda137f32917e
davidhoof <DHoof@gmx.de>
2022-04-03 16:44:47 +0200

    changed test to work again

--- searchengine/model_resolution_test/model_resolution_test.py
+++ searchengine/model_resolution_test/model_resolution_test.py
@@ -1,4 +1,5 @@
 import unittest
+
 import onnx
 
 from model_resolution.model_resolution import ModelResolution
@@ -8,18 +9,20 @@ class MyTestCase(unittest.TestCase):
 
     def setUp(self) -> None:
         # IMPORTANT: An onnx file has to be provided to run these tests
-        onnx_model_path = "G:/Users/David/Downloads/mnist-7 (4).onnx"
+        # onnx_model_path = "G:/Users/David/Downloads/mnist-7 (4).onnx"
+        onnx_model_path = "G:/Users/David/Downloads/resnet152-v2-7.onnx"
 
         self.model = onnx.load(onnx_model_path)
         onnx.checker.check_model(self.model)
 
     def test_something(self):
-
-        mr = ModelResolution(self.model)
-        print([node.name for node in mr.longest_path] )
-        print(mr.calculate_minimal_resolution_with_longest_path())
+        mr = ModelResolution(self.model,use_longest_path=True)
+        print([node.name for node in mr.longest_path])
+        print(mr.minimal_resolution)
 
         self.assertEqual(True, True)
+
+
 
 
 if __name__ == '__main__':
