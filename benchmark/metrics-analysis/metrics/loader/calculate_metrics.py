8c3dcca892ecb221a722c01795cf89dc5b75457c
davidhoof <DHoof@gmx.de>
2022-09-03 17:22:11 +0200

    fixed and improved dbscan option for latent_space metrics

--- benchmark/metrics-analysis/metrics/loader/calculate_metrics.py
+++ benchmark/metrics-analysis/metrics/loader/calculate_metrics.py
@@ -3,6 +3,9 @@ import os
 from sklearn.cluster import KMeans, DBSCAN
 from torchconvquality import measure_quality
 from sklearn.metrics import silhouette_score
+from sklearn.neighbors import NearestNeighbors
+from kneed import KneeLocator
+
 import numpy as np
 from metrics.utils import get_children
 
@@ -127,8 +130,7 @@ class LatentSpaceMetrics(Metrics):
         self.cluster_model_name = cluster_model_name
         self.clustering_model_static_kwargs = clustering_model_static_kwargs
 
-    @staticmethod
-    def __get_clustering_model(cluster_model_name):
+    def __get_clustering_model(self, cluster_model_name):
         """
         Gets the cluster model class and the dynamic parameters, which need to initialized while the
         clustering process is running. Like number of cluster etc.
@@ -144,23 +146,51 @@ class LatentSpaceMetrics(Metrics):
             cluster_model = KMeans
 
         if cluster_model_name in ['dbscan', 'DBScan']:
-            cluster_model_dynamic_kwargs = None
+            cluster_model_dynamic_kwargs = {'eps': None}
+            if 'min_samples' not in self.clustering_model_static_kwargs:
+                cluster_model_dynamic_kwargs['min_samples'] = None
             cluster_model = DBSCAN
 
         return cluster_model, cluster_model_dynamic_kwargs
 
-    @staticmethod
-    def __get_dynamic_params(model, cluster_model_dynamic_kwargs):
+    def __get_dynamic_params(self, latent_space, model, cluster_model_dynamic_kwargs):
         """
         Calculates the dynamic parameters given
+        :param latent_space: latent space, which to cluster
         :param model: dynamic model
         :param cluster_model_dynamic_kwargs: dynamic parameters, which to be initialized
         :return: initialized dynamic parameters as dict
         """
         result = {}
-        if cluster_model_dynamic_kwargs is not None and 'n_clusters' in cluster_model_dynamic_kwargs and cluster_model_dynamic_kwargs['n_clusters'] is None:
+        if cluster_model_dynamic_kwargs is not None:
             result.update(cluster_model_dynamic_kwargs)
+
+        if cluster_model_dynamic_kwargs is not None and 'n_clusters' in cluster_model_dynamic_kwargs and \
+                cluster_model_dynamic_kwargs['n_clusters'] is None:
             result['n_clusters'] = int(model.myhparams['num_classes'])
+
+        if cluster_model_dynamic_kwargs is not None and 'eps' in cluster_model_dynamic_kwargs and \
+                cluster_model_dynamic_kwargs['eps'] is None:
+
+            if self.clustering_model_static_kwargs is not None and 'min_samples' in self.clustering_model_static_kwargs:
+                min_samples = self.clustering_model_static_kwargs['min_samples']
+            else:
+                min_samples = len(np.shape(latent_space)) * 2
+
+            nearest_neighbors = NearestNeighbors(n_neighbors=min_samples)
+            neighbors = nearest_neighbors.fit(latent_space)
+
+            distances, indices = neighbors.kneighbors(latent_space)
+            distances = np.sort(distances[:, min_samples - 1], axis=0)
+
+            i = np.arange(len(distances))
+            knee = KneeLocator(i, distances, S=1, curve='convex', direction='increasing', interp_method='polynomial')
+
+            result['eps'] = distances[knee.knee]
+
+        if cluster_model_dynamic_kwargs is not None and 'min_samples' in cluster_model_dynamic_kwargs and \
+                cluster_model_dynamic_kwargs['min_samples'] is None:
+            result['min_samples'] = len(np.shape(latent_space)) * 2
 
         return result
 
@@ -195,15 +225,24 @@ class LatentSpaceMetrics(Metrics):
 
         latent_space = hook.pull()[0].detach()
 
-        print(model.myhparams['classifier'])
-        print(np.shape(latent_space))
+        print(f'{"model":13}: {model.myhparams["classifier"]}')
+
+        print(f'{"shape":13}: {tuple(np.shape(latent_space))}')
+        print(f'{"cluster_algo":13}: {self.cluster_model_name}')
 
         all_params = {}
 
         clustering_model_class, dynamic_cluster_kwargs = self.__get_clustering_model(self.cluster_model_name)
 
         all_params.update(self.clustering_model_static_kwargs)
-        all_params.update(self.__get_dynamic_params(model, dynamic_cluster_kwargs))
+        all_params.update(self.__get_dynamic_params(
+            latent_space,
+            model,
+            dynamic_cluster_kwargs
+        ))
+        if 'dbscan' == self.cluster_model_name and 'eps' in all_params and 'min_samples' in all_params:
+            print(f'{"eps":13}: {all_params["eps"]:2.2f}')
+            print(f'{"min_samples":13}: {all_params["min_samples"]}')
 
         cluster_model = clustering_model_class(**all_params)
         cluster_model.fit_predict(latent_space)
